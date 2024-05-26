b4756ce89310c2dfe143b0b57ed9429c31537e5f
Motormary <mat.moen@gmail.com>
2024-05-26 20:48:19 +0200

    fixed author email + comments for post js

--- js/functions/404.mjs
+++ js/functions/404.mjs
@@ -1,5 +1,6 @@
 const mainEl = document.querySelector("main")
 
+// Replace main html with error and message from backend if recieved, else show a 404
 export function createErrorPage(errorCode = 404, message = "Page could not be found") {
     mainEl.innerHTML = `
     <div style="margin: auto; text-align: center; padding: 20%;">
--- js/pages/post.mjs
+++ js/pages/post.mjs
@@ -11,14 +11,14 @@ const descriptionEl = document.querySelector("p.date-type")
 const textBodyEl = document.querySelector("div.post-text")
 const authorAvatarEl = document.querySelector("img.author-avatar")
 const authorNameEl = document.querySelector("p.author-name")
-const authorTitleEl = document.querySelector("p.author-title")
+const authorTitleEl = document.querySelector("a.author-title")
 const morePostContainerEl = document.querySelector("div.other-posts")
 const morePostsEl = document.querySelector("div.more-container")
 const headtitleEl = document.querySelector("title")
 const auth = getAuth()
 
 async function getPost() {
-  const url = new URLSearchParams(window.location.search)
+  const url = new URLSearchParams(window.location.search) // Get params from URL
   const postId = url.get("postId")
   const author = url.get("author")
 
@@ -27,26 +27,26 @@ async function getPost() {
   if (data?.data?.id) {
     populatePost(data.data)
   } else {
-    createErrorPage(data.data, "This is not the post you are looking for")
+    createErrorPage(data.data, "This is not the post you are looking for") // If error - display error code and message from server
   }
 }
 
 getPost()
 
 async function populatePost(data) {
-  const postDate = formateDateTime(data.created)
-  const postBody = data.body.split("\n")
-  const otherPosts = await getAllPosts({
+  const postDate = formateDateTime(data.created) // Format date to readable
+  const postBody = data.body.split("\n") // Create array of paragraphs
+  const otherPosts = await getAllPosts({ // Get more posts from the author
     author: data.author.name,
     limit: 4
   })
 
   let newBody = ""
   postBody.forEach((paragraph) => {
-    if (paragraph) return (newBody += `<p>${paragraph}</p>`)
+    if (paragraph) return (newBody += `<p>${paragraph}</p>`) // Put each body-paragraph in it's own paragraph-element and return as html
   })
 
-  bannerEL.src = data?.media?.url || "/assets/images/b1.png"
+  bannerEL.src = data?.media?.url || "/assets/images/filler.png"
   titleEl.textContent = data.title
   descriptionEl.textContent = `${
     data?.tags[0] ? data.tags[0] + " - " : ""
@@ -54,27 +54,30 @@ async function populatePost(data) {
   textBodyEl.innerHTML = newBody
   authorAvatarEl.src = data.author.avatar.url
   authorNameEl.textContent = data.author.name
-  authorTitleEl.textContent = "Manager"
+  authorTitleEl.textContent = data.author.email
+  authorTitleEl.href = `mailto:${data.author.email}`
+  authorTitleEl.setAttribute("title", data.author.email) // Set the email in the title attribute in case it overflows
   headtitleEl.textContent = data.title
 
-  if (auth) {
+  if (auth) { // If user has auth, update href for edit button
     hrefEl.href = `/post/edit.html?author=${data.author.name}&postId=${data.id}`
   } else hrefEl.remove()
 
-  if (otherPosts && otherPosts.data.length > 1) {
-
+  if (otherPosts && otherPosts.data.length > 1) { // If the user has more than 1 post, create section with "Other posts from this author"
     populateOtherPosts(otherPosts.data, data.id)
   }
 }
 
 function populateOtherPosts(otherPosts, currentPostId) {
+  // Make container visible
   morePostContainerEl.removeAttribute("hidden")
   let posts = ""
 
   otherPosts.forEach((post) => {
-    const bodySnippet = post.body.split(".") || [post.body.slice(0, 25)] || [""]
-    if (post.id !== currentPostId)
-      // Avoid showing current post as "other"
+    // Create a snippet from the first line in the body
+    // If no hard stops, slice the body, if no body, create empty array to avoid error
+    const bodySnippet = post.body.split(".") || [post.body.slice(0, 100)] || [""]
+    if (post.id !== currentPostId) // Avoid showing current post as "other"
       return (posts += `
             <a href="/post/?author=${post.author.name}&postId=${post.id}">
                 <p class="other-title">${post.title}</p>
