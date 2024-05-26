c852442fcac059c1f7ca7d354b190485f731c65f
Motormary <mat.moen@gmail.com>
2024-05-26 21:35:08 +0200

    login js comments

--- js/pages/login.mjs
+++ js/pages/login.mjs
@@ -1,17 +1,18 @@
 import { showToast } from "../components/toast.mjs"
+import removeErrors from "../functions/remove-errors.mjs"
 import { LOGIN_URL } from "../urls.mjs"
 
 const loginForm = document.querySelector("form#loginForm")
-const inputfields = document.querySelectorAll(".inputfield")
+const inputLabels = document.querySelectorAll(".inputfield")
 
 loginForm.addEventListener("submit", loginUser)
 
 async function loginUser(event) {
   event.preventDefault()
-  inputfields.forEach(error => error.classList.remove("error"))
+  removeErrors() // Remove any errors
 
   const formData = new FormData(event.target)
-  const formObject = Object.fromEntries(formData.entries())
+  const formObject = Object.fromEntries(formData.entries()) // Create object from formData
 
   try {
     const response = await fetch(LOGIN_URL, {
@@ -25,16 +26,16 @@ async function loginUser(event) {
     if (response.ok) {
       const responseData = await response.json()
       const data = responseData.data
-      const isSaveSelected = formData.has("save-auth")
+      const isSaveSelected = formData.has("save-auth") // Check if user selected the "Keep me signed in option"
       
-      handleSaveAuth(data, isSaveSelected)
+      handleSaveAuth(data, isSaveSelected) // Save auth
 
       window.location.href = "/"
       
     } else {
-      inputfields.forEach(field => field.classList.add("error"))
+      inputLabels.forEach(field => field.classList.add("error")) // If failed login - add error class to labels
       const errorData = await response.json()
-      showToast(errorData.errors[0].message)
+      showToast(errorData.errors[0].message) // Display a toast with the error
     }
   } catch (e) {
     showToast("Something went wrong, try again or contact support.")
@@ -42,6 +43,7 @@ async function loginUser(event) {
   }
 }
 
+// Save auth to session/local storage depending on save value
 function handleSaveAuth(data, save) {
   if (save) {
     localStorage.hvlAuth = JSON.stringify(data)
